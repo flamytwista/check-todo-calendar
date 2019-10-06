eabf125727944f2c1c5cab76fdf217e85e987e14
Taras Yemtsov <flamytwista@gmail.com>
2019-10-06 20:10:18 +0300

    Validation

--- src/components/TaskCreator/TaskCreator.tsx
+++ src/components/TaskCreator/TaskCreator.tsx
@@ -10,14 +10,37 @@ interface Props {
 }
 
 @Component
-export default class TodoItem extends VueComponent<Props> {
+export default class TaskCreator extends VueComponent<Props> {
 
   @Prop()
   private selectedDate!: Date
 
-  isCreatingNow = false
-  isDataValid = false
+  // isCreatingNow = false
+  isCreatingNow = true    // todo: временно для разработки. Потом удалить.
 
+  form = {
+    time: '',
+    text: ''
+  }
+
+  get isTimeValid(): boolean{
+    // вариант только для например 12:06
+    return /^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$/.test(this.form.time)
+    // вариант для например 12:06 и 12:6
+    // return /^(0[0-9]|1[0-9]|2[0-3]|[0-9]):([0-9]|0[0-9]|[1-5][0-9])$/.test(this.form.time)
+  }
+  get isTextValid(): boolean{
+    return this.form.text.length > 0
+  }
+  get isFormValid(): boolean {
+    return this.isTextValid && this.isTimeValid
+  }
+
+  reset(){
+    this.form.time = ''
+    this.form.text = ''
+    this.isCreatingNow = false
+  }
   render() {
     return (
       <div class={styles.taskCreator}>
@@ -25,10 +48,19 @@ export default class TodoItem extends VueComponent<Props> {
           <button onClick={()=>{this.isCreatingNow = true}}>Создать</button>
         ) : (
           <Fragment>
-            <input type="text" placeholder="13:14"/>
-            <input type="text" placeholder="текст"/>
-            <button onClick={()=>{this.isCreatingNow = false}} >Отмена</button>
-            <button disabled={!this.isDataValid}>Сохранить</button>
+            <input
+              type="text"
+              vModel={this.form.time}
+              placeholder="13:14"
+            />
+            <input
+              type="text"
+              placeholder="текст"
+              disabled={!this.isTimeValid}
+              vModel={this.form.text}
+            />
+            <button onClick={this.reset} >Отмена</button>
+            <button disabled={!this.isFormValid}>Сохранить</button>
           </Fragment>
         )}
 
